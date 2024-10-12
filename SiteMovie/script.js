e4bbb43bb642511ec952a8a17c39ca31a6bf0545
Guilherme Quaresma Paes <107657776+gQuaresmaPaes@users.noreply.github.com>
2024-10-12 10:48:55 -0300

    Commit 1.1

---
+++ SiteMovie/script.js
@@ -0,0 +1,5 @@
+// Alterar o título do H1
+document.getElementById("title").textContent = "Trilogia Matrix 1999 - Filme 1";
+
+// Alterar o título da aba do navegador
+document.title = "Trilogia Matrix 1999 - Filme 1";
